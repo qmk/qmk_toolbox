cd75c82595138d5debd5f8147d1b7c209afcdb2d
Jack Humbert <jack.humb@gmail.com>
2017-09-04 21:03:00 -0400

    update gitignore and remove files that were suppose to be ignored

--- osx/Build/Intermediates/qmk_toolbox.build/Debug/qmk_toolbox.build/Objects-normal/x86_64/AppDelegate.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/j/Downloads/qmk_toolbox/qmk_toolbox/qmk_toolbox/AppDelegate.m \
-  /Users/j/Downloads/qmk_toolbox/qmk_toolbox/qmk_toolbox/AppDelegate.h
--- osx/Build/Intermediates/qmk_toolbox.build/Debug/qmk_toolbox.build/Objects-normal/x86_64/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/j/Downloads/qmk_toolbox/qmk_toolbox/qmk_toolbox/main.m
