bc569c42d24794249f9044bc169017ec8ceeb97d
Danny Nguyen <danny@80pct.com>
2017-10-30 18:46:26 -0400

    Save/load last selected microcontroller

---
+++ osx/qmk_toolbox/Constants.h
@@ -0,0 +1,9 @@
+//
+//  Constants.h
+//  QMK Toolbox
+//
+//  Created by Danny Nguyen on 10/30/17.
+//  Copyright © 2017 QMK. All rights reserved.
+//
+
+extern NSString * const QMKMicrocontrollerKey;
