07167a30d1313a979355adc0ec06cb117403e085
Drashna Jaelre <drashna@live.com>
2019-05-29 15:22:26 -0700

    Additional changes to version increment

--- windows/release.bat
+++ windows/release.bat
@@ -1,2 +1,2 @@
-COPY "QMK Toolbox\bin\Release\qmk_toolbox.exe" "qmk_toolbox.exe"
-COPY "install.exe" "qmk_toolbox_install.exe"
+MOVE "QMK Toolbox\bin\Release\qmk_toolbox.exe" "qmk_toolbox.exe"
+MOVE "install.exe" "qmk_toolbox_install.exe"
