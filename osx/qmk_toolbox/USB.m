e1ca758554dcfef1b9c7de433aa9e0619e44cf84
Danny Nguyen <danny@keeb.io>
2019-06-21 15:21:44 -0400

    Ignore Bluetooth device during port scan

--- osx/qmk_toolbox/USB.m
+++ osx/qmk_toolbox/USB.m
@@ -12,7 +12,7 @@
 #include <IOKit/serial/IOSerialKeys.h>
 #include <IOKit/IOBSD.h>
 
-
+#define FILEPATH_SIZE 64
 #define DEFINE_ITER(type) \
 static io_iterator_t            g##type##AddedIter; \
 static io_iterator_t            g##type##RemovedIter
@@ -160,11 +160,12 @@ static void type##DeviceAdded(void *refCon, io_iterator_t iterator) { \
             [_printer print:[NSString stringWithFormat:@"%@ %@", @(STR(type)), @"device connected"] withType:MessageType_Bootloader]; \
             deviceConnected(type); \
             io_iterator_t serialPortIterator; \
-            char deviceFilePath[64]; \
+            char deviceFilePath[FILEPATH_SIZE]; \
             MyFindModems(&serialPortIterator); \
             MyGetModemPath(serialPortIterator, deviceFilePath, sizeof(deviceFilePath)); \
             if (!deviceFilePath[0]) { \
                 printf("No modem port found.\n"); \
+                [_printer printResponse:@"No modem port found, try again." withType:MessageType_Bootloader]; \
             } else { \
                 [delegate setCaterinaPort:[NSString stringWithFormat:@"%s", deviceFilePath]]; \
                 [_printer printResponse:[NSString stringWithFormat:@"Found port: %s", deviceFilePath] withType:MessageType_Bootloader]; \
@@ -274,22 +275,27 @@ static kern_return_t MyGetModemPath(io_iterator_t serialPortIterator, char *devi
  
             // Convert the path from a CFString to a NULL-terminated C string
             // for use with the POSIX open() call.
+            char testDeviceFilePath[FILEPATH_SIZE];
             result = CFStringGetCString(deviceFilePathAsCFString,
-                                        deviceFilePath,
+                                        testDeviceFilePath,
                                         maxPathSize,
                                         kCFStringEncodingASCII);
             CFRelease(deviceFilePathAsCFString);
  
             if (result)
             {
-                printf("BSD path: %s", deviceFilePath);
-                modemFound = true;
-                kernResult = KERN_SUCCESS;
+                NSString *testDevice = [NSString stringWithUTF8String:testDeviceFilePath];
+                if ([testDevice rangeOfString:@"Bluetooth"].location == NSNotFound) {
+                    memcpy(deviceFilePath, testDeviceFilePath, FILEPATH_SIZE);
+                    printf("BSD path: %s\n", deviceFilePath);
+                    modemFound = true;
+                    kernResult = KERN_SUCCESS;
+                } else {
+                    printf("BSD path (ignored): %s\n", testDeviceFilePath);
+                    continue;
+                }
             }
         }
- 
-        printf("\n");
- 
         // Release the io_service_t now that we are done with it.
         IOObjectRelease(modemService);
     }
