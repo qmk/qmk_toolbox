3bb3c58cda30915b264fcbfccb709093a4439c5d
Ryan <fauxpark@gmail.com>
2021-07-21 17:49:11 +1000

    [Windows] More menu items (#280)

---
+++ windows/QMK Toolbox/BindableToolStripMenuItem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QMK_Toolbox
+{
+    public class BindableToolStripMenuItem : ToolStripMenuItem, IBindableComponent
+    {
+        private BindingContext _bindingContext;
+        private ControlBindingsCollection _dataBindings;
+
+        [Browsable(false)]
+        public BindingContext BindingContext
+        {
+            get
+            {
+                if (_bindingContext == null)
+                {
+                    _bindingContext = new BindingContext();
+                }
+                return _bindingContext;
+            }
+            set
+            {
+                _bindingContext = value;
+            }
+        }
+
+        [Category("Data")]
+        [ParenthesizePropertyName(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public ControlBindingsCollection DataBindings
+        {
+            get
+            {
+                if (_dataBindings == null)
+                {
+                    _dataBindings = new ControlBindingsCollection(this);
+                }
+                return _dataBindings;
+            }
+        }
+    }
+}
