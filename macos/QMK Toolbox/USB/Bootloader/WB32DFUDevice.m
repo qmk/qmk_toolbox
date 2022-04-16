a0eca471d25c2daf1d8767198170d35a0ff4ec13
Joy-mac <chang.li@westberrytech.com>
2022-04-16 15:06:34 +0800

    Added wb32-dfu-updater_cli

--- macos/QMK Toolbox/USB/Bootloader/WB32DFUDevice.m
+++ macos/QMK Toolbox/USB/Bootloader/WB32DFUDevice.m
@@ -14,7 +14,7 @@
 -(void)flashWithMCU:(NSString *)mcu file:(NSString *)file {
     if([[[file pathExtension] lowercaseString] isEqualToString:@"bin"]) {
         [self runProcess:@"wb32-dfu-updater_cli" withArgs:@[@"--toolbox-mode", @"--dfuse-address", @"0x08000000", @"--download", file]];
-    } else if([[[file pathExtension] lowercaseString] isEqualToString:@"bin"]) {
+    } else if([[[file pathExtension] lowercaseString] isEqualToString:@"hex"]) {
         [self runProcess:@"wb32-dfu-updater_cli" withArgs:@[@"--toolbox-mode", @"--download", file]];
     } else {
         [self printMessage:@"This file format is not the type supported by wB32-dFU-updater_cli!" withType:MessageType_Error];
