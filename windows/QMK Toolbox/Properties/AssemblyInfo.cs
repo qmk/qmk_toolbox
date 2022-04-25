a657f499732bbe195b51756b75545524b186ac67
Ryan <fauxpark@gmail.com>
2022-04-25 15:07:25 +1000

    Update copyright year (#352)

--- windows/QMK Toolbox/Properties/AssemblyInfo.cs
+++ windows/QMK Toolbox/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("QMK")]
 [assembly: AssemblyProduct("QMK Toolbox")]
-[assembly: AssemblyCopyright("Copyright © 2021 QMK")]
+[assembly: AssemblyCopyright("Copyright © 2022 QMK")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
