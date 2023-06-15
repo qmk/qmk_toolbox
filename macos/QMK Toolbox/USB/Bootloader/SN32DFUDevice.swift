953ffcc34995e8cb52d49c6f75ce30b648db8746
Dimitris Mantzouranis <d3xter93@gmail.com>
2023-06-15 13:14:29 +0300

    [macos] sn32: format the args on sonixflasher

--- macos/QMK Toolbox/USB/Bootloader/SN32DFUDevice.swift
+++ macos/QMK Toolbox/USB/Bootloader/SN32DFUDevice.swift
@@ -9,9 +9,9 @@ class SN32DFUDevice: BootloaderDevice {
 
     override func flash(_ mcu: String, file: String) {
         if productID == 0x7010 {
-            runProcess("sonixflasher", args: ["-v", "\(vendorID):\(productID)", "-o", "0x200", "-f", file])
+            runProcess("sonixflasher", args: ["-v", String(format: "%04X:%04X", vendorID, productID), "-o", "0x200", "-f", file])
         } else {
-            runProcess("sonixflasher", args: ["-v", "\(vendorID):\(productID)", "-f", file])
+            runProcess("sonixflasher", args: ["-v", String(format: "%04X:%04X", vendorID, productID), "-f", file])
         }
     }
 }
