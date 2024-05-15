1e63e81032d516001f84562153d7fe466bd68bdb
Ryan <fauxpark@gmail.com>
2024-05-15 16:05:01 +1000

    [Windows] Improve USB device hardware ID triplet collection (#476)

--- windows/QMK Toolbox/Usb/UsbDevice.cs
+++ windows/QMK Toolbox/Usb/UsbDevice.cs
@@ -29,10 +29,10 @@ namespace QMK_Toolbox.Usb
             ManufacturerString = (string)WmiDevice.GetPropertyValue("Manufacturer");
             ProductString = (string)WmiDevice.GetPropertyValue("Name");
 
-            var hardwareIdTriplet = HardwareIdTripletRegex.Match(GetHardwareId(WmiDevice));
-            VendorId = Convert.ToUInt16(hardwareIdTriplet.Groups[1].ToString(), 16);
-            ProductId = Convert.ToUInt16(hardwareIdTriplet.Groups[2].ToString(), 16);
-            RevisionBcd = Convert.ToUInt16(hardwareIdTriplet.Groups[3].ToString(), 16);
+            var hardwareIdTriplet = GetHardwareId(WmiDevice).Value;
+            VendorId = hardwareIdTriplet.Item1;
+            ProductId = hardwareIdTriplet.Item2;
+            RevisionBcd = hardwareIdTriplet.Item3;
 
             Driver = GetDriverName(WmiDevice);
         }
@@ -42,12 +42,23 @@ namespace QMK_Toolbox.Usb
             return $"{ManufacturerString} {ProductString} ({VendorId:X4}:{ProductId:X4}:{RevisionBcd:X4})";
         }
 
-        private static string GetHardwareId(ManagementBaseObject d)
+        private static (ushort, ushort, ushort)? GetHardwareId(ManagementBaseObject d)
         {
             var hardwareIds = (string[])d.GetPropertyValue("HardwareID");
-            if (hardwareIds != null && hardwareIds.Length > 0)
+            if (hardwareIds != null)
             {
-                return hardwareIds[0];
+                foreach (string hardwareId in hardwareIds)
+                {
+                    Match match = HardwareIdTripletRegex.Match(hardwareId);
+                    if (match.Success)
+                    {
+                        return (
+                            Convert.ToUInt16(match.Groups[1].ToString(), 16),
+                            Convert.ToUInt16(match.Groups[2].ToString(), 16),
+                            Convert.ToUInt16(match.Groups[3].ToString(), 16)
+                        );
+                    }
+                }
             }
 
             return null;
