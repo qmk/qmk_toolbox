c3faee600ca19ccd586f6cca9aade2e73f539150
Nick Brassel <nick@tzarc.org>
2025-09-21 17:51:35 +1000

    Add qmk_flashutils @ 20251011.

--- windows/QMK Toolbox/Helpers/EmbeddedResourceHelper.cs
+++ windows/QMK Toolbox/Helpers/EmbeddedResourceHelper.cs
@@ -21,11 +21,7 @@ namespace QMK_Toolbox.Helpers
             "mdloader.exe",
             "teensy_loader_cli.exe",
             "wb32-dfu-updater_cli.exe",
-            "libftdi1.dll",
-            "libhidapi-0.dll",
-            "libusb-0-1-4.dll",
-            "libusb-1.0.dll",
-            "libwinpthread-1.dll"
+            "flashutils_release_windowsX64"
         };
 
         public static string GetResourceFolder()
